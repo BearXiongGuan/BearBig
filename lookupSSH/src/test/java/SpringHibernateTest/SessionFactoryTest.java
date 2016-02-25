87a674f92292081de59e2b8c5798ed61a1f59246
BearXiongGuan <1171312877@qq.com>
2016-02-25 15:53:06 +0800

    网断了可以提交吗

--- lookupSSH/src/test/java/SpringHibernateTest/SessionFactoryTest.java
+++ lookupSSH/src/test/java/SpringHibernateTest/SessionFactoryTest.java
@@ -1,16 +1,17 @@
 package SpringHibernateTest;
+
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class SessionFactoryTest {
-	 private ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");  
-     @Test  
-     public void testSession()  
-     {           
-               SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");       
-               System.out.println("spring和hibernate整合成功");  
-     }        
+	private ApplicationContext context = new ClassPathXmlApplicationContext(
+			"applicationContext.xml");
+
+	@Test
+	public void testSession() {
+		SessionFactory sf = (SessionFactory) context.getBean("sessionFactory");
+		System.out.println("spring和hibernate整合成功");
+	}
 }
